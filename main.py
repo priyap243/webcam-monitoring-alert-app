bfaa3806bf32b1e0da6095e2074a2e08b10bf8cf
priyap243 <priyap2403@gmail.com>
2023-07-19 15:24:43 +0100

    implement Threading

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import os
 import glob
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -50,8 +51,12 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
-        clean_folder()
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
 
     print(status_list)
 
@@ -62,4 +67,6 @@ while True:
         break
 
 video.release()
+
+clean_thread.start()
 
