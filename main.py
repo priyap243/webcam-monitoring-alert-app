72d111651566b180f08f6bcb635c277d9e00ce64
priyap243 <priyap2403@gmail.com>
2023-07-19 14:56:41 +0100

    Extract image from webcam video and send email attachment of image detected

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 import time
+import os
+import glob
 from emailing import send_email
 
 video = cv2.VideoCapture(0)
@@ -7,6 +9,12 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 
 while True:
     status = 0
@@ -32,12 +40,19 @@ while True:
         rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
+        clean_folder()
+
     print(status_list)
 
     cv2.imshow("Video", frame)
